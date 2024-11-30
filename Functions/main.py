c66d7ad5eec204d5fcdd49db0c36070818fa7d1d
Blexter193 <oleksiy29032005@gmail.com>
2024-12-01 00:26:06 +0200

    lab_5

---
+++ Functions/main.py
@@ -0,0 +1,32 @@
+from Functions.cube import Cube
+from Functions.sphere import Sphere
+from Functions.display_ascii_art import AsciiArtGenerator
+from Functions.user_interface import DisplayInterface
+
+class DisplayInterface:
+    @staticmethod
+    def main():
+        shape_type, size, color = DisplayInterface.get_user_input()
+        shape = None
+
+        if shape_type.lower() == "cube":
+            shape = Cube(size, color)
+        elif shape_type.lower() == "sphere":
+            shape = Sphere(size, color)
+        else:
+            print("Невідома фігура!")
+            return
+
+        generator = AsciiArtGenerator(shape)
+        generator.project_to_2d()
+        generator.display_ascii_art()
+
+        save = input("Бажаєте зберегти результат? (y/n): ")
+        if save.lower() == "y":
+            filename = input("Введіть ім'я файлу для збереження: ")
+            generator.save_to_file(filename)
+            print(f"Результат збережено у файл {filename}")
+
+
+def main():
+    return None
